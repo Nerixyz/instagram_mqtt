99767ff392d3fd9fec08f6673a522308687211a6
Nerixyz <nero.9@hotmail.de>
2020-01-21 18:19:59 +0100

    updated types

--- src/realtime/messages/message-sync.message.ts
+++ src/realtime/messages/message-sync.message.ts
@@ -1,9 +1,18 @@
-import { IrisParserData } from '../parsers/iris.parser';
+import { IrisParserData } from '../parsers';
 import { BigInteger } from '../../shared';
 
 export interface MessageSyncMessageWrapper extends Partial<IrisParserData> {
     message: MessageSyncMessage;
 }
+
+export type MessageSyncMessageType =
+    'text'
+    | 'media'
+    | 'raven_media'
+    | 'voice_media'
+    | 'animated_media'
+    | 'media_share'
+    | string;
 
 export interface MessageSyncMessage {
     thread_id: string;
@@ -12,7 +21,7 @@ export interface MessageSyncMessage {
     item_id: string;
     user_id: BigInteger;
     timestamp: BigInteger;
-    item_type: 'text' | 'media' | 'raven_media' | 'voice_media' | 'animated_media' | string;
+    item_type: MessageSyncMessageType;
     // specific (nullable)
     text?: string;
     media?: RegularMediaItem;
@@ -28,6 +37,7 @@ export interface MessageSyncMessage {
     };
     animated_media?: AnimatedMediaItem;
     visual_media?: VisualMedia;
+    media_share?: MediaShareItem;
 }
 
 export interface ImageVersions {
@@ -39,9 +49,18 @@ export interface ImageVersions {
     }[];
 }
 
+export interface VideoVersions {
+    type: number;
+    width: number;
+    height: number;
+    url: string;
+    id: string;
+}
+
 export interface RegularMediaItem {
     id: string;
-    image_versions2: ImageVersions;
+    image_versions2?: ImageVersions;
+    video_versions?: VideoVersions[];
     original_width: number;
     original_height: number;
     media_type: number;
@@ -56,6 +75,73 @@ export interface RegularMediaItem {
         type: 'TYPE' | string;
         name: 'Type' | string;
     };
+}
+
+export interface MediaShareItem {
+    taken_at: number;
+    pk: number;
+    id: string;
+    device_timestamp: number;
+    media_type: number;
+    code: string;
+    client_cache_key: string;
+    filter_type: number;
+    image_versions2: ImageVersions;
+    video_versions: VideoVersions;
+    original_width: number;
+    original_height: number;
+    user: User;
+    can_viewer_reshare: boolean;
+    caption_is_edited: boolean;
+    comment_likes_enabled: boolean;
+    comment_threading_enabled: boolean;
+    has_more_comments: boolean;
+    max_num_visible_preview_comments: number;
+    can_view_more_preview_comments: boolean;
+    comment_count: number;
+    like_count: number;
+    has_liked: boolean;
+    photo_of_you: boolean;
+    caption: Caption;
+    can_viewer_save: boolean;
+    organic_tracking_token: string;
+}
+
+export interface Caption {
+    pk: number;
+    user_id: number;
+    text: string;
+    type: number;
+    created_at: number;
+    created_at_utc: number;
+    content_type: string;
+    status: string;
+    bit_flags: number;
+    user: User;
+    did_report_as_spam: boolean;
+    share_enabled: boolean;
+    media_id: number;
+}
+
+export interface User {
+    pk: number;
+    username: string;
+    full_name: string;
+    is_private: boolean;
+    profile_pic_url: string;
+    profile_pic_id: string;
+    friendship_status: FriendshipStatus;
+    has_anonymous_profile_picture: boolean;
+    is_unpublished: boolean;
+    is_favorite: boolean;
+    latest_reel_media: number;
+}
+
+export interface FriendshipStatus {
+    following: boolean;
+    outgoing_request: boolean;
+    is_bestie: boolean;
+    is_restricted: boolean;
 }
 
 export interface VisualMedia extends ReplayableMediaItem {
