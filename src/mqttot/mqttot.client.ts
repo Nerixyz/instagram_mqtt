6249912ab186990e1764c4c0658bf479473971a7
Nerixyz <nero.9@hotmail.de>
2020-02-05 19:59:13 +0100

    feat(mqttot): set initial keepAlive

--- src/mqttot/mqttot.client.ts
+++ src/mqttot/mqttot.client.ts
@@ -18,6 +18,7 @@ export class MQTToTClient extends MqttClient {
         this.connectPayload = options.payload;
         this.mqttotDebug(`Creating client`);
         this.registerListeners();
+        this.state.connectOptions = {keepAlive: 60};
     }
 
     protected registerListeners() {
