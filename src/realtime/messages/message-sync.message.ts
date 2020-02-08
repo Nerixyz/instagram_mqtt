881c60d89c29bb07edae761295a93906832fb40a
Nerixyz <nero.9@hotmail.de>
2020-02-08 19:10:18 +0100

    types: added message types from web (the same)

--- src/realtime/messages/message-sync.message.ts
+++ src/realtime/messages/message-sync.message.ts
@@ -5,14 +5,44 @@ export interface MessageSyncMessageWrapper extends Partial<IrisParserData> {
     message: MessageSyncMessage;
 }
 
-export type MessageSyncMessageType =
-    | 'text'
-    | 'media'
-    | 'raven_media'
-    | 'voice_media'
-    | 'animated_media'
-    | 'media_share'
-    | string;
+export enum MessageSyncMessageTypes {
+    DELETION = 'deletion',
+    MEDIA = 'media',
+    TEXT = 'text',
+    LIKE = 'like',
+    HASHTAG = 'hashtag',
+    PROFILE = 'profile',
+    MEDIA_SHARE = 'media_share',
+    LOCATION = 'location',
+    ACTION_LOG = 'action_log',
+    TITLE = 'title',
+    USER_REACTION = 'user_reaction',
+    HISTORY_EDIT = 'history_edit',
+    REACTION_LOG = 'reaction_log',
+    REEL_SHARE = 'reel_share',
+    DEPRECATED_CHANNEL = 'deprecated_channel',
+    LINK = 'link',
+    RAVEN_MEDIA = 'raven_media',
+    LIVE_VIDEO_SHARE = 'live_video_share',
+    TEST = 'test',
+    STORY_SHARE = 'story_share',
+    REEL_REACT = 'reel_react',
+    LIVE_INVITE_GUEST = 'live_invite_guest',
+    LIVE_VIEWER_INVITE = 'live_viewer_invite',
+    TYPE_MAX = 'type_max',
+    PLACEHOLDER = 'placeholder',
+    PRODUCT = 'product',
+    PRODUCT_SHARE = 'product_share',
+    VIDEO_CALL_EVENT = 'video_call_event',
+    POLL_VOTE = 'poll_vote',
+    FELIX_SHARE = 'felix_share',
+    ANIMATED_MEDIA = 'animated_media',
+    CTA_LINK = 'cta_link',
+    VOICE_MEDIA = 'voice_media',
+    STATIC_STICKER = 'static_sticker',
+    AR_EFFECT = 'ar_effect',
+    SELFIE_STICKER = 'selfie_sticker',
+}
 
 export interface MessageSyncMessage {
     thread_id: string;
@@ -21,7 +51,7 @@ export interface MessageSyncMessage {
     item_id: string;
     user_id: BigInteger;
     timestamp: BigInteger;
-    item_type: MessageSyncMessageType;
+    item_type: MessageSyncMessageTypes;
     // specific (nullable)
     text?: string;
     media?: RegularMediaItem;
