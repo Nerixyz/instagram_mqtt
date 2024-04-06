adca63ffa5d21281493e42e720ea01965b6dd762
Debanjan Das <120879267+Debanjan-San@users.noreply.github.com>
2024-04-06 14:39:00 +0530

    Update realtime.client.ts

--- src/realtime/realtime.client.ts
+++ src/realtime/realtime.client.ts
@@ -66,7 +66,7 @@ export class RealtimeClient extends EventEmitter<ToEventFn<RealtimeClientEvents>
     private constructConnection() {
         const userAgent = this.ig.state.appUserAgent;
         const deviceId = this.ig.state.phoneId;
-        const password = `sessionid=${this.ig.state.extractCookieValue('sessionid')}`;
+        const password = `sessionid=${this.ig.state.parsedAuthorization.sessionid}`;
         this.connection = new MQTToTConnection({
             clientIdentifier: deviceId.substring(0, 20),
             clientInfo: {
